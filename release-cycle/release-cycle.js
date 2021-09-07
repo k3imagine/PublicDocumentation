2db39eb3a5f3e6a45655895bf48e914767fc0a2e
sismithk3 <si.smith@k3retail.com>
2021-09-07 11:28:04 +0100

    Update release-cycle.js

--- release-cycle/release-cycle.js
+++ release-cycle/release-cycle.js
@@ -1,47 +1,47 @@
-let originalReleaseDate = new Date('2021-01-12')
+let originalReleaseDate = new Date("2021-01-12");
 let nextProdRelease = originalReleaseDate;
 let prodRelease = true;
-let rcRelease = true
+let rcRelease = true;
 
-function getNextYearsWorthOfReleases(originalReleaseDate){
-    let nextDate = new Date(originalReleaseDate);
-    nextDate.setDate(nextDate.getDate() + 14);
-    originalReleaseDate=nextDate 
-    return nextDate
+function getNextYearsWorthOfReleases(originalReleaseDate) {
+  let nextDate = new Date(originalReleaseDate);
+  nextDate.setDate(nextDate.getDate() + 14);
+  originalReleaseDate = nextDate;
+  return nextDate;
 }
 
-for(let i=0; i<25;i++){
-    if(i%2){
-        prodRelease = false
-        rcRelease = true
-    } else {
-        prodRelease = true
-        rcRelease = false
-    }
-    nextDate = getNextYearsWorthOfReleases(nextProdRelease)
-    nextProdRelease = nextDate
-    addRow(prodRelease, nextProdRelease)
+for (let i = 0; i < 25; i++) {
+  if (i % 2) {
+    prodRelease = false;
+    rcRelease = true;
+  } else {
+    prodRelease = true;
+    rcRelease = false;
+  }
+  nextDate = getNextYearsWorthOfReleases(nextProdRelease);
+  nextProdRelease = nextDate;
+  addRow(prodRelease, nextProdRelease);
 }
 
 function addRow(prodRelease, nextDate) {
-    const todaysDate = Date.now();
-    var yesterday = new Date();
-    yesterday.setDate(yesterday.getDate() - 1);
-    if(nextDate<yesterday) return
-    const div = document.createElement('div');
-    if(prodRelease){
-        div.className = 'container right';
-    }else{
-        div.className = 'container left'
-    }
-    const formattedDate = formatDate(nextDate)
-    let smokeComplete = new Date(nextDate);
-    let uatStarts = new Date(nextDate);
-    smokeComplete.setDate(nextDate.getDate() + 3);
-    uatStarts.setDate(nextDate.getDate() + 6);
-    smokeComplete = formatDate(smokeComplete)
-    uatStarts = formatDate(uatStarts)
-    if(!prodRelease){
+  const todaysDate = Date.now();
+  var yesterday = new Date();
+  yesterday.setDate(yesterday.getDate() - 1);
+  if (nextDate < yesterday) return;
+  const div = document.createElement("div");
+  if (prodRelease) {
+    div.className = "container right";
+  } else {
+    div.className = "container left";
+  }
+  const formattedDate = formatDate(nextDate);
+  let smokeComplete = new Date(nextDate);
+  let uatStarts = new Date(nextDate);
+  smokeComplete.setDate(nextDate.getDate() + 3);
+  uatStarts.setDate(nextDate.getDate() + 6);
+  smokeComplete = formatDate(smokeComplete);
+  uatStarts = formatDate(uatStarts);
+  if (!prodRelease) {
     div.innerHTML = `
         <div class="content">
         <h2 class="rc">RC Release</h2>
@@ -50,51 +50,73 @@ function addRow(prodRelease, nextDate) {
         <p>UAT testing for Hypercare can commence: Morning of ${uatStarts}</p>
         </div>
     `;
-    }else{
-      div.innerHTML = `
+  } else {
+    div.innerHTML = `
         <div class="content">
         <h2 class="prod">Production Release</h2>
         <h3>Evening of ${formattedDate}</h3>
         <p></p>
         </div>
     `;
-    }
-    document.getElementById('content').appendChild(div);
-  }
-
-  function getOrdinal(d) {
-    if (d > 3 && d < 21) return 'th';
-    switch (d % 10) {
-      case 1:  return "st";
-      case 2:  return "nd";
-      case 3:  return "rd";
-      default: return "th";
+    if (formattedDate == "7th September 2021") {
+      div.innerHTML = `
+        <div class="content">
+        <h2 class="prod">Production Release</h2>
+        <h3>Evening of ${formattedDate}</h3>
+        <p>Postponed due to technical issues</p>
+        </div>
+    `;
     }
   }
-  
-  function getMonthName(m){
-    switch (m) {
-      case 0:  return "January";
-      case 1:  return "February";
-      case 2:  return "March";
-      case 3:  return "April";
-      case 4:  return "May";
-      case 5:  return "June";
-      case 6:  return "July";
-      case 7:  return "August";
-      case 8:  return "September";
-      case 9:  return "October";
-      case 10:  return "November";
-      default: return "December";
-    }
+  document.getElementById("content").appendChild(div);
+}
+
+function getOrdinal(d) {
+  if (d > 3 && d < 21) return "th";
+  switch (d % 10) {
+    case 1:
+      return "st";
+    case 2:
+      return "nd";
+    case 3:
+      return "rd";
+    default:
+      return "th";
   }
+}
 
-  function formatDate(d){
-    const ordinal = getOrdinal(d.getDate())
-    const month = getMonthName(d.getMonth());
-    const formattedDate = `${d.getDate()}${ordinal} ${month} ${d.getFullYear()}`
-    return formattedDate
+function getMonthName(m) {
+  switch (m) {
+    case 0:
+      return "January";
+    case 1:
+      return "February";
+    case 2:
+      return "March";
+    case 3:
+      return "April";
+    case 4:
+      return "May";
+    case 5:
+      return "June";
+    case 6:
+      return "July";
+    case 7:
+      return "August";
+    case 8:
+      return "September";
+    case 9:
+      return "October";
+    case 10:
+      return "November";
+    default:
+      return "December";
   }
+}
 
-
-
+function formatDate(d) {
+  const ordinal = getOrdinal(d.getDate());
+  const month = getMonthName(d.getMonth());
+  const formattedDate = `${d.getDate()}${ordinal} ${month} ${d.getFullYear()}`;
+  return formattedDate;
+}
